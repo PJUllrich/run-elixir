85dd2599d9185c259c03f1226912b48e62e73e8c
Peter Ullrich <PJUllrich@users.noreply.github.com>
2024-09-16 18:11:20 +0200

    Add new file logger

--- assets/archived_code/file_logger.ex
+++ assets/archived_code/file_logger.ex
@@ -5,8 +5,20 @@ defmodule Demo.FileLogger do
   """
 
   @default_path "./out.log"
-  @default_format "$time [$level] $message\n"
   @default_level :debug
+  @default_format "$date $time [$level] $message\n"
+
+  def convert_format(format) do
+    Regex.split(~r/(\$\w+)/, format, include_captures: true)
+    |> Enum.map(fn
+      "$" <> "message" -> :msg
+      "$" <> "time" -> :time_short
+      "$" <> word -> String.to_existing_atom(word)
+      "" -> nil
+      other -> other
+    end)
+    |> Enum.reject(&is_nil/1)
+  end
 
   def adding_handler(config) do
     # Merge the config with the default options
@@ -14,12 +26,10 @@ defmodule Demo.FileLogger do
     default_opts = %{path: @default_path, level: @default_level, format: @default_format}
     opts = Map.merge(default_opts, opts)
 
-    formatter = Logger.default_formatter(format: opts.format)
-    opts = Map.put(opts, :formatter, formatter)
+    opts = Map.put(opts, :format, convert_format(opts.format))
 
     # Open the file for writing
-    file = opts.path |> Path.expand() |> File.open!([:append, :utf8])
-    opts = Map.put(opts, :file, file)
+    opts = Map.put(opts, :path, Path.expand(opts.path))
 
     config = Map.put(config, :config, opts)
     {:ok, config}
@@ -30,19 +40,18 @@ defmodule Demo.FileLogger do
     IO.inspect(opts, label: "Config")
 
     if :logger.compare_levels(event.level, opts.level) != :lt do
-      {:string, message} = event.msg
+      {{year, month, day}, {hour, minute, second}} =
+        :calendar.system_time_to_universal_time(event.meta.time, :microsecond)
 
-      line = opts.formatter.format(message) |> IO.inspect()
+      date = ~c"#{year}-#{month}-#{day}"
+      time_short = ~c"#{hour}:#{minute}:#{second}"
+      event = %{event | meta: Map.merge(event.meta, %{date: date, time_short: time_short})}
 
-      IO.binwrite(opts.file, line)
+      line =
+        :logger_formatter.format(event, %{template: opts.format}) |> Enum.join()
+
+      File.write!(opts.path, line, [:append, :utf8])
     end
-
-    :ok
-  end
-
-  def remove_handler(%{config: opts}) do
-    # Close the file before the handler is removed from the application
-    File.close(opts.path)
 
     :ok
   end
