4c3b1acd3b35d1dff400e6e0252b0ae3aa4cca7b
Peter Ullrich <PJUllrich@users.noreply.github.com>
2024-09-24 11:20:42 +0200

    Add publish action

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 cp _headers ./doc/_headers
-mix publishex.netlify --token $NETLIFY_ACCESS_TOKEN --site-id $NETLIFY_SITE_ID
+
+until mix publishex.netlify --token $NETLIFY_ACCESS_TOKEN --site-id $NETLIFY_SITE_ID; do
+    echo "Command failed, retrying..."
+done
